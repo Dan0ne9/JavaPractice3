87ea580737f7f60e4acaf173f6e8d0ee3b09d1b0
dan0ne9 <daniil.vishnivetskii0610@yandex.ru>
2024-01-29 15:24:57 +0300

    fixBugs

--- src/main/java/com/example/finalproject/person/config/SecurityConfiguration.java
+++ src/main/java/com/example/finalproject/person/config/SecurityConfiguration.java
@@ -2,14 +2,12 @@ package com.example.finalproject.person.config;
 
 import com.example.finalproject.person.service.UserService;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,13 +15,11 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
     @Bean
     public static BCryptPasswordEncoder passwordEncoder() {
@@ -43,16 +39,6 @@ public class SecurityConfiguration {
                 ).formLogin(
                         form -> form
                                 .loginPage("/login")
-=======
-                      //  authorize.requestMatchers("/registration**").permitAll()
-                      //          .requestMatchers("/js/**").permitAll()
-                       //         .requestMatchers("/css/**").permitAll()
-                        //        .requestMatchers("/img/**").permitAll()
-            //    ).formLogin(
-                     //   form -> form
-                           //     .loginPage("/login")
-                               // .permitAll()
-
                 ).logout(
                         logout -> logout
                                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
